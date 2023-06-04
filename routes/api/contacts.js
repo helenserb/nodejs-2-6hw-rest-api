6af85dab81b2bee9504da8fd6a980b97aa19ceb4
helenserb <serbinenkoifbb@gmail.com>
2023-06-03 21:38:17 -0400

    created get route contactById

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -17,7 +17,22 @@ router.get('/', async (req, res, next) => {
 })
 
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const { contactId } = req.params;
+    const result = await contactsService.getContactById(contactId);
+    if (!result) {
+      const error = newError("Not Found");
+      error.code = 404;
+      throw error;
+          }
+          res.json(result);
+        }
+  catch (error) {
+    const { status = 500, message = "Server error" } = error; 
+      res.status(status).json({
+        message,
+      });
+    }
 })
 
 router.post('/', async (req, res, next) => {
