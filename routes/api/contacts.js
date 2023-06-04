5d783a40d6e15041fb99491c9808467afd3ccc66
helenserb <serbinenkoifbb@gmail.com>
2023-06-03 21:11:49 -0400

    add actions with contacts

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,8 +2,11 @@ const express = require('express')
 
 const router = express.Router()
 
+const contactsService = require("../../models/contacts")
+
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const result = await contactsService.listContacts();
+  res.json(result)
 })
 
 router.get('/:contactId', async (req, res, next) => {
