a99d9d87b1ed56c00b0d5874e6996fc438c628a4
helenserb <serbinenkoifbb@gmail.com>
2023-06-03 21:19:02 -0400

    created add route

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -5,8 +5,15 @@ const router = express.Router()
 const contactsService = require("../../models/contacts")
 
 router.get('/', async (req, res, next) => {
-  const result = await contactsService.listContacts();
-  res.json(result)
+  try {
+      const result = await contactsService.listContacts();
+      res.json(result)
+  }
+  catch (error){
+    res.status(500).json({
+      message: error.message,
+    })
+  }
 })
 
 router.get('/:contactId', async (req, res, next) => {
