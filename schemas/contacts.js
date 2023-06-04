a0ad3572d1eda3716954820b47929fc983026856
helenserb <serbinenkoifbb@gmail.com>
2023-06-04 10:42:34 -0400

    editted phone validation

--- schemas/contacts.js
+++ schemas/contacts.js
@@ -4,7 +4,7 @@ const contactAddSchema = Joi.object({
   name: Joi.string().required(),
   email: Joi.string().required(),
   phone: Joi.string()
-    .regex(/^[0-9]{10}$/)
+    .regex(/^(\d{3}) \d{3}-\d{4}$/)
     .messages({ "string.pattern.base": `Phone number must have 10 digits.` })
     .required(),
 });
