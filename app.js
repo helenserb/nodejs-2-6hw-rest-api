5ada1da2cbe333a7f76a88aff8a389cb7b38272f
helenserb <serbinenkoifbb@gmail.com>
2023-06-19 19:42:20 -0400

    iniit authRouter

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require('express')
 const logger = require('morgan')
 const cors = require('cors')
 
-const contactsRouter = require('./routes/api/contacts')
+const authRouter = require("./routes/api/auth-routes");
+const contactsRouter = require('./routes/api/contacts');
+
 
 const app = express()
 require("dotenv").config();
@@ -13,6 +15,7 @@ app.use(logger(formatsLogger))
 app.use(cors())
 app.use(express.json())
 
+app.use("/api/auth", authRouter);
 app.use('/api/contacts', contactsRouter)
 
 app.use((req, res) => {
