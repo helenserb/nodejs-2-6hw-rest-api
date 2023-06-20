40e6b447ea4035967db4f5cccd711ba9a9fa3872
helenserb <serbinenkoifbb@gmail.com>
2023-06-20 18:40:37 -0400

    pagination

--- controllers/contacts-controller.js
+++ controllers/contacts-controller.js
@@ -4,7 +4,9 @@ const {ctrlWrapper} = require("../decorators")
 
 const getAllContacts = async (req, res) => {
   const { _id: owner } = req.user;
-    const result = await Contact.find({owner}).populate("owner");
+  const { page = 1, limit = 10, ...query } = req.query;
+  const skip = (page - 1) * limit;
+    const result = await Contact.find({owner, ...query}, {skip, limit}).populate("owner");
     res.json(result);
 };
 
