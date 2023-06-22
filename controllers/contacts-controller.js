018dab110677bb354b752ade28b80afde7d8d11f
helenserb <serbinenkoifbb@gmail.com>
2023-06-21 21:53:15 -0400

    filter by favorite

--- controllers/contacts-controller.js
+++ controllers/contacts-controller.js
@@ -4,9 +4,9 @@ const {ctrlWrapper} = require("../decorators")
 
 const getAllContacts = async (req, res) => {
   const { _id: owner } = req.user;
-  const { page = 1, limit = 20 } = req.query;
+  const { page = 1, limit = 20, ...query} = req.query;
   const skip = (page - 1) * limit;
-  const result = await Contact.find({owner},"",{skip, limit}).populate("owner", "email");
+  const result = await Contact.find({owner, ...query},"",{skip, limit}).populate("owner", "email");
 
   res.json(result);
 };
